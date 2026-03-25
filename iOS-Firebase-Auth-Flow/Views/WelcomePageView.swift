27d33bf3e35f0b21a916f0b4363471c663dfaaef
Amel <amelsb45@gmail.com>
2026-03-25 14:08:25 -0400

    Add anonymous sign-in function

--- iOS-Firebase-Auth-Flow/Views/WelcomePageView.swift
+++ iOS-Firebase-Auth-Flow/Views/WelcomePageView.swift
@@ -6,10 +6,11 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct WelcomePageView: View {
     @Environment(AuthenticationManager.self) var authManager
-    
+ 
     var body: some View {
         NavigationStack {
             ZStack {
@@ -28,17 +29,22 @@ struct WelcomePageView: View {
                         )
                         .foregroundStyle(Color.primaryIcon)
                     Spacer()
-                    Button {
-                        //TODO: SignIn Anonymously function
+                    if authManager.authState == .signedOut {
+                        Button {
+                            Task{
+                                await signInAnonymously()
+                            }
 
-                    } label: {
-                        Text("Skip")
-                            .font(.body.bold())
-                            .foregroundStyle(Color.primaryIcon)
-                            .frame(width: 280, height: 45, alignment: .center)
+                        } label: {
+                            Text("Skip")
+                                .font(.body.bold())
+                                .foregroundStyle(Color.primaryIcon)
+                                .frame(width: 280, height: 45, alignment: .center)
+                        }.padding()
                     }
+                  
 
-                    .padding()
+                    
                     Text("Sign in to continue 🤗")
                 }
             }.navigationTitle("Welcome")
@@ -49,4 +55,16 @@ struct WelcomePageView: View {
     WelcomePageView()
         .environment(AuthenticationManager())
 
+}
+
+extension WelcomePageView {
+    
+    func signInAnonymously() async {
+        do {
+                   let result = try await authManager.signInAnonymously()
+                   print("SignInAnonymouslySuccess: \(result.user.uid)")
+               } catch {
+                   print("SignInAnonymouslyError: \(error.localizedDescription)")
+               }
+    }
 }
